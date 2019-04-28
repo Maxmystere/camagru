fe1495ee4e7e346115512f770e01d65ef7f2e8ff
Maxence GRAB <magrab@e2r5p4.42.fr>
2019-04-28 20:10:46 +0200

    Must remove ajax from code

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 	<meta charset="utf-8" />
 	<title>Camagru</title>
 	<link rel="stylesheet" type="text/css" href="/index.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script type="text/javascript" src="index.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
